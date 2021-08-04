10f010dce26c2437fae936c5d0b59a92aa0f4a96
Santosh Pillai <sapillai@redhat.com>
2021-08-04 20:25:12 +0530

    added fetcher cli bootstrap
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -28,15 +29,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "marketmoz",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "MarketMoz is an algo trading application",
+	Long: `
+MarketMoz is a CLI based algo trading application
+for intraday trading and scalping on NSE and BSE market (India).`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
